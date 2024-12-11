7e32a29bfa857071aeb2128a7231d7b42a632be9
Carlos Henrique klz <97392902+kralluz@users.noreply.github.com>
2024-12-11 10:36:59 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
+module.exports = {
+  webpack: (config: any, { dev }: any) => {
+    if (dev) {
+      config.devtool = "source-map"; 
+    }
+    return config;
   },
 };
-
-export default nextConfig;
